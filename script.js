ce56980ecdd759911143a3c3925bf397c1fb55cd
qqpdb <153456436+qqpdb@users.noreply.github.com>
2025-12-26 18:18:49 -0800

    Add getComputerChoice function
    This function uses Math.random to choose rock, paper, or scissors, and
    then returns the choice as a string.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+
+function getComputerChoice() {
+    //create variable to store random number
+    let roll;
+    //create variable to store string
+    let compHand;
+    //generate random number and store it into variable
+    roll = Math.random();
+    //if the number is less than or equal to .33, assign rock to string variable
+    //if the number is greater than or equal to .34, AND less than or equal to .66, assign paper to string variable
+    //if the number is greater than or equal to .67, assign scissors to string variable
+    if (roll <= .33) {
+        compHand = "Rock";
+    } else if (roll >= .34 && roll <= .66) {
+        compHand = "Paper";
+    } else {
+        compHand = "Scissors";
+    }
+    return compHand;
+}
+
+console.log(getComputerChoice());
